425d437a6f98e27e1fb86e3b01d002192897b345
lyraguilherme <74629816+lyraguilherme@users.noreply.github.com>
2022-12-28 09:00:27 -0300

    Update LyraCollector.py

--- LyraCollector.py
+++ LyraCollector.py
@@ -17,21 +17,22 @@ def collector(device_ip, device_port, ssh_user, ssh_pass):
 
     with ConnectHandler(**params) as ch:
 
-        print(f"\n[!] Conexao estabelecida com {device_ip}")
-        # seta terminal length para evitar erros com outputs extensos     
+        print(f"\n[!] Connection established with {device_ip}")
+        
+        # set terminal length to avoid issues with longer outputs
         ch.send_command('terminal length 0')
 
-        # identifica hostname
+        # identify device hostname
         hostnameraw = ch.send_command('show running-config | include hostname')
         hostname = hostnameraw.split()
         hostname = hostname[1]
 
-        # define nome do arquivo que sera criado com base no hostname do equipamento
+        # set output .txt filename based on device hostname
         filename = f"{hostname}.txt"
         print(f"--> Hostname: {hostname}")
 
-        # coleta informacoes   
-        print("--> Coletando informacoes...")
+        # connect to device and collect information
+        print("--> Collecting information...")
         output = "#show version\n" + ch.send_command('show version') + "\n\n\n"
         output += "#show inventory\n" + ch.send_command('show inventory') + "\n\n\n"
         output += "#show modules\n" + ch.send_command('show modules') + "\n\n\n"
@@ -43,27 +44,22 @@ def collector(device_ip, device_port, ssh_user, ssh_pass):
         output += "#show ip route\n" + ch.send_command('show ip route') + "\n\n\n"
         output += "#show vrf\n" + ch.send_command('show vrf') + "\n\n\n"        
 
-        # grava outputs em arquivo
-        print("--> Gravando informacoes...")
+        # write outputs on file
+        print("--> Writing file...")
         f = open(filename, "w")
         f.write("-" * 30 + "\n" + hostname + "\n" + "-" * 30 + "\n\n")
         f.write(output)
-        print("--> Criado arquivo " + filename)
+        print("--> File saved: " + filename)
 
         # encerra conexao
         ch.disconnect()
-        print("[!] Conexao encerrada.\n")
+        print("[!] Connection closed.\n")
 
 def main():
     parser = ArgumentParser(description='')
-    parser.add_argument('-c', '--csv', required=True, help='arquivo CSV')    
-
-    # credenciais
-    #ssh_user = input('\nSSH Username: ')
-    #ssh_pass = getpass.getpass('SSH Password: ')
-    #device_port = "22"
-
-    # csv
+    parser.add_argument('-c', '--csv', required=True, help='CSV filename')    
+    
+    # csv handling
     csv_file = parser.parse_args().csv
     with open(csv_file, mode='r') as devices_list:
         csv_reader = csv.DictReader(devices_list, delimiter=',')
